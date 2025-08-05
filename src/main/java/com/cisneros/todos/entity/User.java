445b936f4aee0916dafe95bb00717626a55c6e82
jgacisneros <jgacisneros@hotmail.com>
2025-08-05 11:56:28 -0700

    Collection Table for Authorities

--- src/main/java/com/cisneros/todos/entity/User.java
+++ src/main/java/com/cisneros/todos/entity/User.java
@@ -35,11 +35,19 @@ public class User implements UserDetails {
     @Column(name = "updated_at")
     private Date updatedAt;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_authorities", joinColumns = @JoinColumn(name = "used_id"))
+    private List<Authority> authorities;
+
     public User() {}
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
+    }
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
     }
 
     @Override
