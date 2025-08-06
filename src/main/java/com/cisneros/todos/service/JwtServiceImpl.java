8c8365df017b4d2b297049bbcb53c33563a5d8c3
jgacisneros <jgacisneros@hotmail.com>
2025-08-06 13:39:27 -0700

    Updating generateToken method

--- src/main/java/com/cisneros/todos/service/JwtServiceImpl.java
+++ src/main/java/com/cisneros/todos/service/JwtServiceImpl.java
@@ -1,9 +1,15 @@
 package com.cisneros.todos.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
 import java.util.Map;
 
 @Service
@@ -27,6 +33,18 @@ public class JwtServiceImpl implements JwtService {
 
     @Override
     public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
-        return null;
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+JWT_EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .compact();
+
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
