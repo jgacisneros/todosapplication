5af66dd9c869fa1cfbe7edbddabec6466dd86a30
jgacisneros <jgacisneros@hotmail.com>
2025-08-07 12:56:16 -0700

    Authentication controller register

---
+++ src/main/java/com/cisneros/todos/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.cisneros.todos.controller;
+
+import com.cisneros.todos.request.RegisterRequest;
+import com.cisneros.todos.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@Tag(name = "Authentication TEST API Endpoints", description = "Operations related to register and login")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Operation(summary = "Register a user", description = "Create new user in database")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/register")
+    public void register(@Valid @RequestBody RegisterRequest registerRequest) throws Exception {
+        authenticationService.register(registerRequest);
+    }
+}
