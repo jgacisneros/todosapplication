efcd43794f469b6e37f7233d5cd0c0c811e40026
jgacisneros <jgacisneros@hotmail.com>
2025-08-07 09:54:45 -0700

    User repository implementation

---
+++ src/main/java/com/cisneros/todos/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cisneros.todos.repository;
+
+import com.cisneros.todos.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
