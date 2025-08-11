396568112c88fb7e9fffabf5a02ba7c256ab0bf5
jgacisneros <jgacisneros@hotmail.com>
2025-08-11 13:15:54 -0700

    Adding tag and response for user controller

--- src/main/java/com/cisneros/todos/controller/UserController.java
+++ src/main/java/com/cisneros/todos/controller/UserController.java
@@ -3,8 +3,10 @@ package com.cisneros.todos.controller;
 import com.cisneros.todos.request.PasswordUpdateRequest;
 import com.cisneros.todos.response.UserResponse;
 import com.cisneros.todos.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User REST API Endpoints", description = "Operations related to info about current user")
@@ -18,16 +20,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "User information", description = "Get current user info")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/info")
     public UserResponse getUserInfo() {
         return userService.getUserInfo();
     }
 
+    @Operation(summary = "Delete user", description = "Delete current user account")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping
     public void deleteUser() {
         userService.deleteUser();
     }
 
+    @Operation(summary = "Password update", description = "Change user password after verification")
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/password")
     public void passwordUpdate(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest) throws Exception {
         userService.updatePassword(passwordUpdateRequest);
