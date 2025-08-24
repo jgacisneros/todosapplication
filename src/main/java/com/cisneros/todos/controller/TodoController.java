4262f86f0fe4c285b1c5e3591bfbdb5adf981bc4
jgacisneros <jgacisneros@hotmail.com>
2025-08-24 08:33:14 -0700

    Todo controller logic

--- src/main/java/com/cisneros/todos/controller/TodoController.java
+++ src/main/java/com/cisneros/todos/controller/TodoController.java
@@ -6,8 +6,11 @@ import com.cisneros.todos.service.TodoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Todo REST API Endpoints", description = "Operations for managing user todos")
 @RestController
@@ -19,11 +22,31 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @Operation(summary = "Get all todos for user", description = "Fetch all todo from signed in user")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public List<TodoResponse> getAllTodos() {
+        return todoService.getAllTodos();
+    }
 
     @Operation(summary = "Create todo for user", description = "Create todo for the signed in user")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public TodoResponse createTodo(@Valid @RequestBody TodoRequest todoRequest) {
         return todoService.createTodo(todoRequest);
+    }
+
+    @Operation(summary = "Update todo for user", description = "Update todo for the signed in user")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public TodoResponse toggleTodoCompletion(@PathVariable @Min(1) long id) {
+        return todoService.toggleTodoCompletion(id);
+    }
+
+    @Operation(summary = "Delete todo for user", description = "Delete todo for the signed in user")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable @Min(1) long id) {
+        todoService.deleteTodo(id);
     }
 }
