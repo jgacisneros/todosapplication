494bf3e198188655cf273dc900fca3a0e69a3c60
jgacisneros <jgacisneros@hotmail.com>
2025-08-24 13:20:17 -0700

    Admin controllers

--- src/main/java/com/cisneros/todos/config/SecurityConfig.java
+++ src/main/java/com/cisneros/todos/config/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
                 configurer
                         .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**",
                                 "/swagger-resources/**", "/webjars/**", "/docs").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
         );
 
